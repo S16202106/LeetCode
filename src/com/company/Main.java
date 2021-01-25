64e9335f36df574fc6d81f02f727c1527ae57240
S16202106 <758850574@qq.com>
2021-01-25 15:01:49 +0800

    二叉树的中序遍历

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ import com.company.tree.TreeNode;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root= new TreeNode();
-        root.val=1;
-        root.right=new TreeNode(2);
-        root.right.left=new TreeNode(3);
-        Solution solution=new Solution();
+        TreeNode root = new TreeNode();
+        root.val = 1;
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        Solution solution = new Solution();
         solution.inorderTraversal(root);
     }
 }
--- src/com/company/tree/Solution.java
+++ src/com/company/tree/Solution.java
@@ -3,28 +3,29 @@ package com.company.tree;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
+
 // 二叉树的中序遍历
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack=new Stack<TreeNode>();
-        TreeNode head=root;
-        List<Integer> res=new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode head = root;
+        List<Integer> res = new LinkedList<>();
         // 入栈时 必须遍历根节点的左子树
-        while(null != head){
+        while (null != head) {
             stack.push(head);
-            head=head.left;
+            head = head.left;
         }
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             // 出栈时 判断右子树 是否存在
-            head=stack.pop();
+            head = stack.pop();
             res.add(head.val);
-            if(null != head.right){
-                head=head.right;
+            if (null != head.right) {
+                head = head.right;
                 stack.push(head);
-                head=head.left;
-                while(null != head){
+                head = head.left;
+                while (null != head) {
                     stack.push(head);
-                    head=head.left;
+                    head = head.left;
                 }
             }
         }
