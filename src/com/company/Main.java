bf1aaa51b646c37d5e5668cb1908718ba3feb137
S16202106 <758850574@qq.com>
2021-01-27 17:52:10 +0800

    连通性

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,26 @@ package com.company;
 
 import com.company.tree.Solution;
 import com.company.tree.TreeNode;
+import org.omg.CORBA.INTERNAL;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+    //    public static void main(String[] args) {
+//        TreeNode root = new TreeNode();
+//        root.val = 1;
+//        root.right = new TreeNode(2);
+//        root.right.left = new TreeNode(3);
+//        Solution solution = new Solution();
+//        solution.inorderTraversal(root);
+//    }
     public static void main(String[] args) {
-        TreeNode root = new TreeNode();
-        root.val = 1;
-        root.right = new TreeNode(2);
-        root.right.left = new TreeNode(3);
-        Solution solution = new Solution();
-        solution.inorderTraversal(root);
+        String s="\\/";
+        int [] t=new int[2];
+        System.out.print(s.charAt(0));
+        Map<Integer, Integer> maps=new HashMap<>();
+        maps.containsKey(1);
     }
 }
---
+++ src/com/company/graph/ping/Union.java
@@ -0,0 +1,30 @@
+package com.company.graph.ping;
+
+public class Union {
+    // id[i] 表示连通分量的编号
+    public int [] id;
+    public int count;
+
+    // 初始化n个触点
+    public Union(int n){
+        id=new int[n];
+        for(int i=0;i<n;i++)
+            id[i]=i;
+    }
+    // 在点p和q之间添加一条连接
+    void union(int p,int q){
+        id[q]=id[p];
+    }
+    // p(0,n-1)所在分量的标识符
+    int find(int p){
+        return id[p];
+    }
+    // 判断p和q是否存在于同一个分量中
+    boolean connected(int p,int q){
+        return id[p] ==id[q];
+    }
+    // 连通分量的数量
+    int count(){
+        return count;
+    }
+}
